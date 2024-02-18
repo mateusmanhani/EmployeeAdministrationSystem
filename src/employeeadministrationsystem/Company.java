ca6447ea11630c77ad53590ba709c32963108d09
Mateus Manhani <58527872+mateusmanhani@users.noreply.github.com>
2024-02-18 15:19:33 +0000

    create overloaded constructor to accept company name as parameter

--- src/employeeadministrationsystem/Company.java
+++ src/employeeadministrationsystem/Company.java
@@ -1,13 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package employeeadministrationsystem;
+
+import java.util.ArrayList;
 
 /**
  *
- * @author Mateus
+ * @author Mateus Manhani
  */
 public class Company {
+    private String companyName;
+    private ArrayList<Employee> staff;
+    
+    // Default constractor that initializes all fields
+    public Company(){
+        this.companyName = "Name Example";
+        this.staff = new ArrayList<Employee>();
+    }
+    
+    // Overloaded constructor
+    public Company(String name){
+        this.companyName = name;
+        this.staff = new ArrayList<Employee>();
+    }
     
 }
