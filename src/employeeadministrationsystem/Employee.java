4c0d72189bfd8db02d1c6b1988c01c1669bd1b9e
Mateus Manhani <58527872+mateusmanhani@users.noreply.github.com>
2024-02-18 14:19:37 +0000

    create constructors for Employee
    One constructor that takes name and email as parameters and another with no parameter and sets default values to name and email of employee

--- src/employeeadministrationsystem/Employee.java
+++ src/employeeadministrationsystem/Employee.java
@@ -6,5 +6,24 @@ package employeeadministrationsystem;
  * @author Mateus Manhani
  */
 public class Employee {
+    private String name;
+    private String email;
+    private int empNum;
+    private static int nextEmpNum = 1;
+    
+    // Constructor with parameter
+    public Employee(String name, String email) {
+        this.name = name;
+        this.email = email;
+        this.empNum = nextEmpNum++; //auto increment empNum to have unique employee numbers
+    }
+    
+    // Constructor with default values
+    public Employee(){
+        this.name = "John Doe";
+        this.name = "john.doe@example.com";
+        this.empNum = nextEmpNum++; //auto increment empNum to have unique employee numbers
+    }
+    
     
 }
---
+++ src/employeeadministrationsystem/EmployeeTest.java
@@ -0,0 +1,14 @@
+
+package employeeadministrationsystem;
+
+/**
+ *
+ * @author Mateus Manhani
+ */
+public class EmployeeTest {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
