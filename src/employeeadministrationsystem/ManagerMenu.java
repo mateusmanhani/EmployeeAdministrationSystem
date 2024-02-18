d981fc14543cb5fe2b806722e520ec37d7bc8bd9
Mateus Manhani <58527872+mateusmanhani@users.noreply.github.com>
2024-02-18 20:19:44 +0000

    create isValidManager
    in this method we check weather the manager login is valid to add and remove staff later on

--- src/employeeadministrationsystem/ManagerMenu.java
+++ src/employeeadministrationsystem/ManagerMenu.java
@@ -1,13 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package employeeadministrationsystem;
+
+import java.util.Scanner;
 
 /**
  *
- * @author Mateus
+ * @author Mateus Manhani
  */
 public class ManagerMenu {
+    private static final String MANAGER_USERNAME = "Gnomeo"; //valid username to give access
+    private static final String MANAGER_PASSWORD = "smurf"; // valid password to give access
     
+    public static void start(){
+        Scanner scanner = new Scanner(System.in);
+        
+        //Manager login
+        System.out.println("");
+    }
+    
+    private static boolean isValidManager(Scanner scanner){
+        System.out.println("Enter username: ");
+        String username = scanner.nextLine();
+        System.out.println("Enter password: ");
+        String password = scanner.nextLine();
+        return username.equals(MANAGER_USERNAME) && password.equals(MANAGER_PASSWORD);
+    }
 }
