de9c0a1607864233f47e3662288a8c72de53936d
Mateus Manhani <58527872+mateusmanhani@users.noreply.github.com>
2024-02-19 10:53:43 +0000

    implement case 2 on displayMenu method
    case 2 addNewStaff, ask the manager for input on the employee he/she would like to add to the company's staff, in this case name and email, insatiate a new employee and add it to the company staff

--- src/employeeadministrationsystem/ManagerMenu.java
+++ src/employeeadministrationsystem/ManagerMenu.java
@@ -12,20 +12,20 @@ public class ManagerMenu {
     private static final String CORRECT_PASSWORD = "smurf"; // valid password to give access
     
     
-    public static void start(Manager manager){
+    public static void start(Manager manager, Company company){
         Scanner scanner = new Scanner(System.in);
         
         //Manager login
         System.out.println("Welcome to the Manager Console Menu");
         if (manager != null && isAuthenticated(manager,CORRECT_USERNAME,CORRECT_PASSWORD)){
             // if login successful, display menu
-            displayMenu(scanner);
+            displayMenu(scanner, company);
         }else {
             System.out.println("Invalid username or password. Exiting...");
         }
     }
     
-    private static void displayMenu(Scanner scanner){
+    private static void displayMenu(Scanner scanner, Company company){
         //Display menu options
         System.out.println("\nMenu:");
         System.out.println("1. View current Staff");
@@ -37,9 +37,18 @@ public class ManagerMenu {
         switch (choice){
             case 1:
                 //View current staff
+                
                 break;
             case 2:
+                // Ask manager for input on the new employee to add to the staff
+                System.out.println("Enter the new employee name: ");
+                String newEmpName = scanner.nextLine();
+                System.out.println("Enter the new employee email: ");
+                String newEmpEmail = scanner.nextLine();
+                //Instatiate new employee
+                Employee newEmp = new Employee(newEmpName,newEmpEmail);
                 // add new staff
+                company.addNewStaff(newEmp);
                 break;
             case 3:
                 //exit
