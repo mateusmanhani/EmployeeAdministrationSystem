cf32ea219e34ad948ac3b50ee259500b94ed2387
Mateus Manhani <58527872+mateusmanhani@users.noreply.github.com>
2024-02-19 11:23:27 +0000

    removeStaff method in company class
    in this method I used an iterator to go through the set comparing employee number to find the desired employee to remove from the set

--- src/employeeadministrationsystem/Company.java
+++ src/employeeadministrationsystem/Company.java
@@ -28,7 +28,22 @@ public class Company {
     public void addNewStaff(Employee newEmp){
         staffSet.add(newEmp);
     }
-    
+    public void removeStaff(int empNum){
+        
+        //create new iterator in order to iterate through staffset and check each employee's empNum against the one given as parameter
+        Iterator<Employee> iterator = staffSet.iterator();
+        while (iterator.hasNext()){
+            Employee employee = iterator.next();
+            if(employee.getEmpNum() == empNum){
+                iterator.remove();// remove employee from the set
+                System.out.println("Employee with employee number " + empNum + " removed succesfully.");
+                return;// exit method after removing employee
+            }
+        }
+        
+        // If the employee with the given empNum is not found
+        System.out.println("Employee with employee number " + empNum + " not found in the staff");
+    }
     // get number of employees
     public int getStaffNumber(){
         return staffSet.size();
