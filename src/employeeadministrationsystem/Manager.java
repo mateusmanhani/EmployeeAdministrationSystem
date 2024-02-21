ba3655df5bbce7025764425fce14398856fc4854
Mateus Manhani <58527872+mateusmanhani@users.noreply.github.com>
2024-02-21 21:47:35 +0000

    Update Manager.java

--- src/employeeadministrationsystem/Manager.java
+++ src/employeeadministrationsystem/Manager.java
@@ -43,6 +43,6 @@ public class Manager extends Employee {
 //            ", username='" + username + '\'' +
 //            ", password='" + password + '\'' +
 //            '}';
-    }
+//    }
     
 }
